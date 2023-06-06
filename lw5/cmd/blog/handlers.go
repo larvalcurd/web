aff3185202a919f6e1c2af533c4e5af9a3a6a102
larvalcurd <110689984+larvalcurd@users.noreply.github.com>
2023-06-06 21:03:33 +0300

    :0

--- lw5/cmd/blog/handlers.go
+++ lw5/cmd/blog/handlers.go
@@ -13,7 +13,7 @@ type indexPage struct {
 
 type featuredPostData struct {
 	Title     string
-	Subtitle  string
+	SubTitle  string
 	Author    string
 	AuthorImg string
 	PubDate   string
@@ -22,7 +22,7 @@ type featuredPostData struct {
 
 type recentPostData struct {
 	Title         string
-	Subtitle      string
+	SubTitle      string
 	RecentPostImg string
 	Author        string
 	AuthorImg     string
@@ -31,7 +31,7 @@ type recentPostData struct {
 
 type postPage struct {
 	Title    string
-	Subtitle string
+	SubTitle string
 	Text     []postTexting
 }
 
@@ -72,7 +72,7 @@ func post(w http.ResponseWriter, r *http.Request) {
 
 	data := postPage{
 		Title:    "The Road Ahead",
-		Subtitle: "The road ahead might be paved - it might not be.",
+		SubTitle: "The road ahead might be paved - it might not be.",
 		Text:     postTextes(),
 	}
 
@@ -90,18 +90,18 @@ func featuredPosts() []featuredPostData {
 	return []featuredPostData{
 		{
 			Title:     "The Road Ahead",
-			Subtitle:  "The road ahead might be paved - it might not be.",
-			PostImg:   "/static/image/the-road-ahead-post.png",
+			SubTitle:  "The road ahead might be paved - it might not be.",
+			PostImg:   "/static/images/the-road-ahead-post.png",
 			Author:    "Mat Vogels",
-			AuthorImg: "/static/image/mat-vogels.png",
+			AuthorImg: "/static/images/mat-vogels.png",
 			PubDate:   "September 25, 2015",
 		},
 		{
 			Title:     "From Top Down",
-			Subtitle:  "Once a year, go someplace you’ve never been before.",
-			PostImg:   "/static/image/from-top-to-down.png",
+			SubTitle:  "Once a year, go someplace you’ve never been before.",
+			PostImg:   "../static/images/from-top-to-down.png",
 			Author:    "William Wong",
-			AuthorImg: "/static/image/william-wong.png",
+			AuthorImg: "../static/images/william-wong.png",
 			PubDate:   "September 25, 2015",
 		},
 	}
@@ -111,50 +111,50 @@ func recentPosts() []recentPostData {
 	return []recentPostData{
 		{
 			Title:         "Still Standing Tall",
-			Subtitle:      "Life begins at the end of your comfort zone.",
-			RecentPostImg: "/static/image/still-standing-tall.png",
+			SubTitle:      "Life begins at the end of your comfort zone.",
+			RecentPostImg: "../static/images/still-standing-tall.png",
 			Author:        "William Wong",
-			AuthorImg:     "/static/image/william-wong.png",
+			AuthorImg:     "../static/images/william-wong.png",
 			PubDate:       "9/25/2015",
 		},
 		{
 			Title:         "Sunny Side Up",
-			Subtitle:      "No place is ever as bad as they tell you it’s going to be.",
-			RecentPostImg: "/static/image/sunny-side-up.png",
+			SubTitle:      "No place is ever as bad as they tell you it’s going to be.",
+			RecentPostImg: "../static/images/sunny-side-up.png",
 			Author:        "Mat Vogels",
-			AuthorImg:     "/static/image/mat-vogels.png",
+			AuthorImg:     "../static/images/mat-vogels.png",
 			PubDate:       "9/25/2015",
 		},
 		{
 			Title:         "Water Falls",
-			Subtitle:      "We travel not to escape life, but for life not to escape us.",
-			RecentPostImg: "/static/image/water-falls.png",
+			SubTitle:      "We travel not to escape life, but for life not to escape us.",
+			RecentPostImg: "../static/images/water-falls.png",
 			Author:        "Mat Vogels",
-			AuthorImg:     "/static/image/mat-vogels.png",
+			AuthorImg:     "../static/images/mat-vogels.png",
 			PubDate:       "9/25/2015",
 		},
 		{
 			Title:         "Through the Mist",
-			Subtitle:      "Travel makes you see what a tiny place you occupy in the world.",
-			RecentPostImg: "/static/image/through-the-mist.png",
+			SubTitle:      "Travel makes you see what a tiny place you occupy in the world.",
+			RecentPostImg: "../static/images/through-the-mist.png",
 			Author:        "William Wong",
-			AuthorImg:     "/static/image/william-wong.png",
+			AuthorImg:     "../static/images/william-wong.png",
 			PubDate:       "9/25/2015",
 		},
 		{
 			Title:         "Awaken Early",
-			Subtitle:      "Not all those who wander are lost.",
-			RecentPostImg: "/static/image/awaken-early.png",
+			SubTitle:      "Not all those who wander are lost.",
+			RecentPostImg: "../static/images/awaken-early.png",
 			Author:        "Mat Vogels",
-			AuthorImg:     "/static/image/mat-vogels.png",
+			AuthorImg:     "../static/images/mat-vogels.png",
 			PubDate:       "9/25/2015",
 		},
 		{
 			Title:         "Try it Always",
-			Subtitle:      "The world is a book, and those who do not travel read only one page.",
-			RecentPostImg: "/static/image/try-it-always.png",
+			SubTitle:      "The world is a book, and those who do not travel read only one page.",
+			RecentPostImg: "../static/images/try-it-always.png",
 			Author:        "Mat Vogels",
-			AuthorImg:     "/static/image/mat-vogels.png",
+			AuthorImg:     "../static/images/mat-vogels.png",
 			PubDate:       "9/25/2015",
 		},
 	}
@@ -163,7 +163,7 @@ func recentPosts() []recentPostData {
 func postTextes() []postTexting {
 	return []postTexting{
 		{
-			Texting: "Dark spruce forest frowned on either side the frozen waterway. \n \nTThe trees had been stripped by a recent wind of their white covering of frost, and they seemed to lean towards each other, black and ominous, in the fading light. A vast silence reigned over the land. The land itself was a desolation, lifeless, without movement, so lone and cold that the spirit of it was not even that of sadness. There was a hint in it of laughter, but of a laughter more terrible than any sadness—a laughter that was mirthless as the smile of the sphinx, a laughter cold as the frost and partaking of the grimness of infallibility. It was the masterful and incommunicable wisdom of eternity laughing at the futility of life and the effort of life. \n It was the Wild, the savage, frozen-hearted Northland Wild. But there was life, abroad in the land and defiant. Down the frozen waterway toiled a string of wolfish dogs. Their bristly fur was rimed with frost. Their breath froze in the air as it left their mouths, spouting forth in spumes of vapour that settled upon the hair of their bodies and formed into crystals of frost. Leather harness was on the dogs, and leather traces attached them to a sled which dragged along behind. The sled was without runners. It was made of stout birch-bark, and its full surface rested on the snow. The front end of the sled was turned up, like a scroll, in order to force down and under the bore of soft snow that surged like a wave before it. On the sled, securely lashed, was a long and narrow oblong box. There were other things on the sled—blankets, an axe, and a coffee-pot and frying-pan; but prominent, occupying most of the space, was the long and narrow oblong box. In advance of the dogs, on wide snowshoes, toiled a man. At the rear of the sled toiled a second man. On the sled, in the box, lay a third man whose toil was over,—a man whom the Wild had conquered and beaten down until he would never move nor struggle again. It is not the way of the Wild to like movement. Life is an offence to it, for life is movement; and the Wild aims always to destroy movement. It freezes the water to prevent it running to the sea; it drives the sap out of the trees till they are frozen to their mighty hearts; and most ferociously and terribly of all does the Wild harry and crush into submission man—man who is the most restless of life, ever in revolt against the dictum that all movement must in the end come to the cessation of movement. But at front and rear, unawed and indomitable, toiled the two men who were not yet dead. Their bodies were covered with fur and soft-tanned leather. Eyelashes and cheeks and lips were so coated with the crystals from their frozen breath that their faces were not discernible. This gave them the seeming of ghostly masques, undertakers in a spectral world at the funeral of some ghost. But under it all they were men, penetrating the land of desolation and mockery and silence, puny adventurers bent on colossal adventure, pitting themselves gainst the might of a world as remote and alien and pulseless as the abysses of space.",
+			Texting: "Dark spruce forest frowned on either side the frozen waterway. \n \nThe trees had been stripped by a recent wind of their white covering of frost, and they seemed to lean towards each other, black and ominous, in the fading light. A vast silence reigned over the land. The land itself was a desolation, lifeless, without movement, so lone and cold that the spirit of it was not even that of sadness. There was a hint in it of laughter, but of a laughter more terrible than any sadness—a laughter that was mirthless as the smile of the sphinx, a laughter cold as the frost and partaking of the grimness of infallibility. It was the masterful and incommunicable wisdom of eternity laughing at the futility of life and the effort of life. \n It was the Wild, the savage, frozen-hearted Northland Wild. But there was life, abroad in the land and defiant. Down the frozen waterway toiled a string of wolfish dogs. Their bristly fur was rimed with frost. Their breath froze in the air as it left their mouths, spouting forth in spumes of vapour that settled upon the hair of their bodies and formed into crystals of frost. Leather harness was on the dogs, and leather traces attached them to a sled which dragged along behind. The sled was without runners. It was made of stout birch-bark, and its full surface rested on the snow. The front end of the sled was turned up, like a scroll, in order to force down and under the bore of soft snow that surged like a wave before it. On the sled, securely lashed, was a long and narrow oblong box. There were other things on the sled—blankets, an axe, and a coffee-pot and frying-pan; but prominent, occupying most of the space, was the long and narrow oblong box. In advance of the dogs, on wide snowshoes, toiled a man. At the rear of the sled toiled a second man. On the sled, in the box, lay a third man whose toil was over,—a man whom the Wild had conquered and beaten down until he would never move nor struggle again. It is not the way of the Wild to like movement. Life is an offence to it, for life is movement; and the Wild aims always to destroy movement. It freezes the water to prevent it running to the sea; it drives the sap out of the trees till they are frozen to their mighty hearts; and most ferociously and terribly of all does the Wild harry and crush into submission man—man who is the most restless of life, ever in revolt against the dictum that all movement must in the end come to the cessation of movement. But at front and rear, unawed and indomitable, toiled the two men who were not yet dead. Their bodies were covered with fur and soft-tanned leather. Eyelashes and cheeks and lips were so coated with the crystals from their frozen breath that their faces were not discernible. This gave them the seeming of ghostly masques, undertakers in a spectral world at the funeral of some ghost. But under it all they were men, penetrating the land of desolation and mockery and silence, puny adventurers bent on colossal adventure, pitting themselves gainst the might of a world as remote and alien and pulseless as the abysses of space.",
 		},
 	}
 }
